c51416c6d3e5c160ff793231c9de0688aff65e2c
Shakya7 <68787887+Shakya7@users.noreply.github.com>
2022-01-27 01:35:17 +0530

    Create viewRoutes.js

---
+++ backend/routes/viewRoutes.js
@@ -0,0 +1,11 @@
+const express=require("express");
+const userController=require("../controllers/userController");
+const noteController=require("../controllers/noteController");
+
+const router=express.Router();
+
+router.route("/login-form").post(userController.login);
+router.route("/signup-form").post(userController.createUser);
+router.route("/get-notes").get(userController.protect,noteController.getAllNotesOfUser);
+
+module.exports=router;
