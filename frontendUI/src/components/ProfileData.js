d1b9a5bb61a8206f914cc9c62f5b1783d989986b
Shakya7 <68787887+Shakya7@users.noreply.github.com>
2022-01-31 02:09:17 +0530

    Create ProfileData.js

---
+++ frontendUI/src/components/ProfileData.js
@@ -0,0 +1,91 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function Profile(props){
+    console.log(props.dataP);
+    const [notes,setNotes]=useState([]);
+    //const [cNote,setCNote]=useState(false);
+    useEffect(()=>{
+        console.log("Notes Uploaded to Client-Side");
+        console.log(notes);
+        props.sendNotes(notes);
+    },[notes]);
+
+    return(
+        <div className="profile-body">
+            <div>
+                <section className="side-section">
+                
+                </section>
+            </div>
+            <div className="show">
+                <div>
+                    <p className="wlcm-txt">Welcome, {props.dataP}</p>
+                </div>
+                <div className="note-create">
+                    <div onClick={async(e)=>{
+                            /*try{
+                                const result=await axios.get("/get-notes");
+                                //console.log(result.data.data.allNotes);
+                                setNotes(result.data.data.allNotes);
+                            }catch(err){
+                                console.log(err);
+                                //props.alertRoute();
+                                props.showResultScreen("error");
+                            }*/
+                            try{
+                                props.createNoteProf();
+                                //setCNote(true);
+                            }catch(err){
+                                console.log(err);
+                                //props.alertRoute();
+                                props.showResultScreen("error");
+                            }
+                        }  
+                    }>+
+                    </div>
+                </div>
+                <div className="profile-main-section">
+                    <div className="notes-sec">
+                        <div className="prof-sub" onClick={async (e)=>{
+                            try{
+                            const result=await axios.get("/get-notes");
+                            //console.log(result.data.data.allNotes);
+                            setNotes(result.data.data.allNotes);
+                            props.showAllNotes();
+                            }catch(err){
+                                console.log(err);
+                                //props.alertRoute();
+                                props.showResultScreen("error");
+                            }
+                        }}>
+                            View all Notes
+                        </div>
+                        <div className="prof-sub" onClick={(e)=>{
+                            try{
+                                props.createNoteProf();
+                                //setCNote(true);
+                            }catch(err){
+                                console.log(err);
+                                //props.alertRoute();
+                                props.showResultScreen("error");
+                            }
+                        }}>
+                            Create a Note
+                        </div>
+                    </div>
+                    <div className="tasks-sec">
+                        <div className="prof-sub">
+                            View all Tasks
+                        </div>
+                        <div className="prof-sub">
+                            Create a Task
+                        </div>
+                    </div>    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
