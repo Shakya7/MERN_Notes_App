0fa4e6117fc5a81c26aa2b48fd09a3a32d7cb961
Shakya7 <68787887+Shakya7@users.noreply.github.com>
2022-02-01 01:28:49 +0530

    Create Footer.js

---
+++ frontendUI/src/components/Footer.js
@@ -0,0 +1,10 @@
+function Footer(){
+    return(
+        <div className="footer-div">
+            <div>
+                <button className="bttn">Link 1</button>
+            </div>
+        </div>
+    )
+}
+export default Footer;
