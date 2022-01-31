3c48b7cb5423ab854cb2aa7e56f030c07bbd38e5
Shakya7 <68787887+Shakya7@users.noreply.github.com>
2022-01-31 06:12:39 +0530

    Update NoteCard.js

--- frontendUI/src/components/user-notes-section/NoteCard.js
+++ frontendUI/src/components/user-notes-section/NoteCard.js
@@ -1,7 +1,7 @@
 function NoteCard(props){
     //console.log(props.action)
     return(
-        <div style={{width:"20vw",height:"auto",backgroundColor:"yellow",borderRadius:"10px 10px 10px 10px",padding:"10px"}}>
+        <div style={{width:"20vw",height:"auto",backgroundColor:"yellow",borderRadius:"10px 10px 10px 10px",padding:"10px", boxShadow: "rgb(106, 84, 116) 5px 5px 8px"}}>
             <div>
                 <p>{props.dataNotes}</p>
             </div>
