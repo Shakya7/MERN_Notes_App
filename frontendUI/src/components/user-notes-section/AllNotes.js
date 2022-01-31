eafaeffb425eb90129853fcc8256b8f26500a7de
Shakya7 <68787887+Shakya7@users.noreply.github.com>
2022-02-01 01:26:02 +0530

    Create AllNotes.js

---
+++ frontendUI/src/components/user-notes-section/AllNotes.js
@@ -0,0 +1,102 @@
+import NoteCard from "./NoteCard";
+
+function AllNotes(props){
+    console.log(props.notes);
+    const notesPresent=props.notes.length;
+    //console.log(notesPresent);
+    return(
+        <div className="all-notes-sec">
+            All Notes
+            <br/>
+            <div>
+                <input type="text" placeholder="Search for notes here" style={{padding:"10px",width:"80vw",height:"5vmin", borderRadius:"15px 15px 15px 15px"}}/>
+            </div>
+            {
+                notesPresent?
+                <div>
+                    <div style={{marginTop:"10px", width:"80vw",height:"65vh",backgroundColor:"violet",borderRadius:"15px 15px 15px 15px", display:"grid",gridTemplateColumns:"1fr 1fr 1fr",gridGap:"20px",overflowY:"auto",scrollbarWidth:"none", padding:"20px",boxShadow: "rgb(106, 84, 116) 5px 5px 8px"}}> 
+                        {
+                            props.notes.map((val,id)=>(
+                                
+                                <NoteCard id={id+1} greenSignal1={props.getNoteID} greenSignal={props.showNote} key={id} dataID={val._id} dataNotes={val.note}/>
+                            ))
+                        }
+                    </div>
+                </div>:"Oops...You dont have any notes created!"
+            }
+            <div style={{
+                cursor: "pointer",
+                width:"50px",
+                height: "50px",
+                borderRadius: "35px",
+                backgroundColor: "rgb(252, 237, 151)",
+                display:"flex",
+                justifyContent: "center",
+                alignItems: "center",
+                color:"rgb(59, 32, 59)",
+                fontSize: "30px",
+                position: "absolute",
+                bottom:"100px",
+                right:"30px",
+            }}>
+                <div onClick={async(e)=>{
+                            /*try{
+                                const result=await axios.get("/get-notes");
+                                //console.log(result.data.data.allNotes);
+                                setNotes(result.data.data.allNotes);
+                            }catch(err){
+                                console.log(err);
+                                //props.alertRoute();
+                                props.showResultScreen("error");
+                            }*/
+                            try{
+                                await props.createNote();
+                            }catch(err){
+                                console.log(err);
+                                //props.alertRoute();
+                                props.showResultScreen("error");
+                            }
+                        }  
+                }>+
+                </div>
+            </div>
+            <div style={{
+                cursor: "pointer",
+                width:"70px",
+                height: "30px",
+                borderRadius: "5px",
+                backgroundColor: "rgb(252, 237, 151)",
+                display:"flex",
+                justifyContent: "center",
+                alignItems: "center",
+                color:"rgb(59, 32, 59)",
+                fontSize: "10px",
+                position: "absolute",
+                top:"80px",
+                left:"30px",
+            }}>
+                <div onClick={async(e)=>{
+                            /*try{
+                                const result=await axios.get("/get-notes");
+                                //console.log(result.data.data.allNotes);
+                                setNotes(result.data.data.allNotes);
+                            }catch(err){
+                                console.log(err);
+                                //props.alertRoute();
+                                props.showResultScreen("error");
+                            }*/
+                            try{
+                                props.goBack();
+                            }catch(err){
+                                console.log(err);
+                                //props.alertRoute();
+                                props.showResultScreen("error");
+                            }
+                        }  
+                }>Go back
+                </div>
+            </div>
+        </div>
+    )
+}
+export default AllNotes;
