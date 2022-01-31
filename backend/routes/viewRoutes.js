8399cf37d7b2a817f24ccdfc0a31a97717d34639
Shakya7 <68787887+Shakya7@users.noreply.github.com>
2022-01-31 06:03:51 +0530

    Update viewRoutes.js

--- backend/routes/viewRoutes.js
+++ backend/routes/viewRoutes.js
@@ -7,5 +7,6 @@ const router=express.Router();
 router.route("/login-form").post(userController.login);
 router.route("/signup-form").post(userController.createUser);
 router.route("/get-notes").get(userController.protect,noteController.getAllNotesOfUser);
+router.route("/create-note").post(userController.protect,noteController.createNoteFromUser);
 
 module.exports=router;
