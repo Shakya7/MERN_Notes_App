068da85b997aae0b7dbc7ed4dec99c1735c6de91
Shakya7 <68787887+Shakya7@users.noreply.github.com>
2022-02-01 01:28:11 +0530

    Create ErrorData.js

---
+++ frontendUI/src/components/ErrorData.js
@@ -0,0 +1,12 @@
+function Error(){
+    return(
+        <div className="error-page">
+            <div>Hello This is Error page</div>
+            <div style={{marginTop:"100px", backgroundColor:"white", width:"50vw",height:"30vh", display:"flex",justifyContent:"center",alignItems:"center", borderRadius:"20px"}}>
+                SOME ERROR HAPPENED!!!
+            </div>
+        </div>
+    )
+}
+
+export default Error;
